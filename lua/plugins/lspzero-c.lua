367c573ff8ca9ec840c2d0d4a5c6fe262bdec7c6
lucianjonesoctane <lucian.jones@octanelending.com>
2024-12-21 12:17:17 -0500

    update config

--- lua/plugins/lspzero-c.lua
+++ lua/plugins/lspzero-c.lua
@@ -30,19 +30,24 @@ return {
         'hrsh7th/nvim-cmp',
         event = 'InsertEnter',
         dependencies = {
-            { 'L3MON4D3/LuaSnip' },
+            {
+                'L3MON4D3/LuaSnip',
+                dependencies = { "rafamadriz/friendly-snippets" }
+            },
             { 'hrsh7th/cmp-nvim-lsp' },
             { 'hrsh7th/cmp-path' },
             { 'hrsh7th/cmp-buffer' },
             { 'saadparwaiz1/cmp_luasnip' },
             { 'onsails/lspkind.nvim' },
             { "roobert/tailwindcss-colorizer-cmp.nvim" },
+            { url = "https://codeberg.org/FelipeLema/cmp-async-path" },
         },
         config = function()
             local cmp = require('cmp')
             local cmp_action = require('lsp-zero.cmp').action()
             local luasnip = require("luasnip")
             local lspkind = require("lspkind")
+            require("luasnip.loaders.from_vscode").lazy_load()
 
             cmp.setup({
                 preselect = 'item',
@@ -70,15 +75,41 @@ return {
                     end
                 },
                 sources = {
+                    { name = 'luasnip' },
                     { name = 'nvim_lsp' },
                     { name = 'path' },
                     { name = 'buffer' },
-                    { name = 'luasnip' },
+                },
+                sorting = {
+                    comparators = {
+                        cmp.config.compare.offset,
+                        cmp.config.compare.exact,
+                        cmp.config.compare.score,
+
+                        function(entry1, entry2)
+                            local _, entry1_under = entry1.completion_item.label:find "^_+"
+                            local _, entry2_under = entry2.completion_item.label:find "^_+"
+                            entry1_under = entry1_under or 0
+                            entry2_under = entry2_under or 0
+                            if entry1_under > entry2_under then
+                                return false
+                            elseif entry1_under < entry2_under then
+                                return true
+                            end
+                        end,
+
+                        cmp.config.compare.kind,
+                        cmp.config.compare.sort_text,
+                        cmp.config.compare.length,
+                        cmp.config.compare.order,
+                    },
                 },
                 mapping = {
                     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                    ['<Tab>'] = cmp_action.luasnip_supertab(),
-                    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+                    ['<Tab>'] = cmp_action.tab_complete(),
+                    ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+                    -- ['<Tab>'] = cmp_action.luasnip_supertab(),
+                    -- ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
                 },
                 formatting = {
                     expandable_indicator = true,
@@ -95,6 +126,8 @@ return {
                                 nvim_lua = "Lua",
                             })
                         })(entry, item)
+
+                        vim.opt.pumheight = 10
 
                         local cmp_kinds = {
                             Text = "",
@@ -141,6 +174,12 @@ return {
                         return fmt
                     end
                 },
+            })
+            cmp.setup.cmdline({ '/', '?' }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
             })
         end
     },
@@ -205,12 +244,12 @@ return {
                 'html',
                 'helm_ls',
                 'jsonls',
-                'tsserver',
                 'lua_ls',
                 'rust_analyzer',
                 'sqlls',
                 'taplo',
                 'terraformls',
+                'ts_ls',
                 'vimls',
                 'yamlls',
                 'zk',
